53b623bd3a38b2a0ec67ef491df6c503a4bc0eef
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-04-01 08:45:29 -0400

    success getting data from the data base and consoling it in ROW1 file, line 9

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import DashboardBox from '@/components/DashboardBox'
-import { useGetKpisQuery } from '@/state/api'
+import React from 'react';
+import DashboardBox from '@/components/DashboardBox';
+import { useGetKpisQuery } from '@/state/api';
 
-type Props = {}
+type Props = {};
 
 const Row1 = (props: Props) => {
-  const {data} = useGetKpisQuery()
-  return (
-<>
-<DashboardBox gridArea='a'></DashboardBox>
-<DashboardBox gridArea='b'></DashboardBox>
-<DashboardBox gridArea='c'></DashboardBox>
-</>
-  )
-}
+  const { data } = useGetKpisQuery();
+  console.log('data', data);
 
-export default Row1
+  return (
+    <>
+      <DashboardBox gridArea='a'></DashboardBox>
+      <DashboardBox gridArea='b'></DashboardBox>
+      <DashboardBox gridArea='c'></DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
