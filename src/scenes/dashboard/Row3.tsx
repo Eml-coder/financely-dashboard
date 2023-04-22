8f8c50537873f079bd56569408999adff9dbad3e
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-04-22 18:37:40 -0400

    implement 3 pie charts by looping over them

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,16 +1,36 @@
 import { DataGrid, GridCellParams } from '@mui/x-data-grid';
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
+import { useMemo } from 'react';
 
 import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/state/api';
 import DashboardBox from '@/components/DashboardBox';
 import BoxHeader from '@/components/BoxHeader';
+import FlexBetween from '@/components/FlexBetween';
+import { Cell, Pie, PieChart } from 'recharts';
+
 
 const Row3 = () => {
   const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[300], palette.primary[100]];
   const { data: kpiData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: transactionData } = useGetTransactionsQuery();
-  console.log(transactionData)
+  console.log(transactionData);
+
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(([key, value]) => {
+        return [
+          { name: key, value: value },
+          {
+            name: `${key} of Total`,
+            value: totalExpenses - value,
+          },
+        ];
+      });
+    }
+  }, [kpiData]);
 
   const productColumns = [
     {
@@ -43,7 +63,6 @@ const Row3 = () => {
       field: 'buyer',
       headerName: 'Buyer',
       flex: 0.67,
-  
     },
 
     {
@@ -63,7 +82,7 @@ const Row3 = () => {
 
   return (
     <>
-    {/* first grid with tables */}
+      {/* first grid with tables */}
       <DashboardBox gridArea='g'>
         <BoxHeader title='list of Products' sideText={`${productData?.length} products`} />
         <Box
@@ -100,7 +119,10 @@ const Row3 = () => {
       {/* second grid with tables */}
       <DashboardBox gridArea='h'>
         {' '}
-        <BoxHeader title='Recent Orders' sideText={`${transactionData?.length} latest transactions`} />
+        <BoxHeader
+          title='Recent Orders'
+          sideText={`${transactionData?.length} latest transactions`}
+        />
         <Box
           mt='1rem'
           p='0 0.5rem'
@@ -132,8 +154,32 @@ const Row3 = () => {
           />
         </Box>
       </DashboardBox>
-      {/* third grid left to right */}
-      <DashboardBox gridArea='i'></DashboardBox>
+      {/* third grid left to right 3 circular charts */}
+      <DashboardBox gridArea='i'>
+        <BoxHeader title='Expense Breakdown By Category' sideText='+5%' />
+        <FlexBetween mt='0.5rem' gap='0.5rem' p='0 1rem' textAlign='center'>
+          {/* loop over the piechart */}
+          {pieChartData?.map((data, i) => (
+            <Box key={`${data[0].name}-${i}`}>
+              <PieChart width={70} height={70}>
+                <Pie
+                  data={data}
+                  stroke='none'
+                  innerRadius={18}
+                  outerRadius={35}
+                  fill='#8884d8'
+                  paddingAngle={2}
+                  dataKey='value'>
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant='h5'>{data[0].name}</Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardBox>
       <DashboardBox gridArea='j'></DashboardBox>
     </>
   );
